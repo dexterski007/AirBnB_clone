45acee64bd905653d10520fc46324250de6bce8b
Dexterski007 <mbouj007@gmail.com>
2024-03-09 11:08:11 -0800

    volcano

--- models/user.py
+++ models/user.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" user model """
+""" user model based on basemodel"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ Users """
+    """ represeentation of user based on BaseModel with mail pass nd names """
     email = ""
     password = ""
     first_name = ""
